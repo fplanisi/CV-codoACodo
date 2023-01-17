04f112d50c09e4adfc3d095de29476c015cfd75d
Franco Planisi <franco.planisi@gmail.com>
2023-01-16 23:35:34 -0300

    HTML + CSS terminado

---
+++ js/code.js
@@ -0,0 +1,9 @@
+// language
+let check = document.querySelector(".check");
+check.addEventListener('click',idioma);
+function idioma(){
+  let id=check.checked;
+    if (id==true){location.href="es/index.html"}
+    else{location.href="../index.html"}}
+
+//
