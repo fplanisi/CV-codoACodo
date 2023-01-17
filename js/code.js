7d93ca4a2e6c3c63921c6d98df9ad3dfdf75326f
Franco Planisi <franco.planisi@gmail.com>
2023-01-17 03:04:52 -0300

    agregadas fx en CVs

--- js/code.js
+++ js/code.js
@@ -6,4 +6,23 @@ function idioma(){
     if (id==true){location.href="es/index.html"}
     else{location.href="../index.html"}}
 
-//
+
+//signature
+function sign() {
+  document.getElementById("signature").style.display = "inline-block";
+  document.querySelector(".btn-s").style.display = "none";
+}
+
+//date
+function startTime() {
+  const d = new Date();
+  document.getElementById('date').innerHTML =  d.toDateString()
+  document.getElementById("date").style.display = "inline";
+  document.querySelector(".btn-d").style.display = "none";
+}
+
+//console.log
+function consoleLog() {
+  document.querySelector(".CV-lado-B__footer-text").style.display = "block";
+  document.querySelector(".CV-lado-B__footer-title").style.display = "block";
+}
